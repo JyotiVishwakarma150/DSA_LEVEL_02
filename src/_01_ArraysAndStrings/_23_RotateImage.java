d5279027a7f79664dbfbf429614b36606c647f6d
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-09-16 00:30:11 +0530

    RotateImage

---
+++ src/_01_ArraysAndStrings/_23_RotateImage.java
@@ -0,0 +1,57 @@
+package _01_ArraysAndStrings;
+import java.util.*;
+public class _23_RotateImage {
+    // ~~~~~~~~~~~~~User Section~~~~~~~~~~~~~
+    public static void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    public static void reverseRow(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix[0].length - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+
+    public static void rotate(int[][] matrix) {
+        // write your code here
+        //1.Transpose
+        transpose(matrix);
+        //2.reverse every row
+        reverseRow(matrix);
+
+
+    }
+
+    // ~~~~~~~~~~~Input Management~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = scn.nextInt();
+            }
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
