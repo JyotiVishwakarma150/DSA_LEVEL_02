4215300d58081f051cbefee672f7de26a4add7f8
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-09-17 23:08:28 +0530

    AddStrings

---
+++ src/_01_ArraysAndStrings/_27_AddStrings.java
@@ -0,0 +1,30 @@
+package _01_ArraysAndStrings;
+import java.util.*;
+public class _27_AddStrings {
+    public static String addStrings(String num1, String num2) {
+        // write your code here
+        String res="";
+        int carry=0;
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        while(i>=0 || j>=0 || carry!=0){
+            //get ival
+            int ival= i>=0?num1.charAt(i)-'0':0;
+            int jval= j>=0?num2.charAt(j)-'0':0;
+            i--;
+            j--;
+            int sum=ival+jval+carry;
+            res=(sum%10)+res;
+            carry = sum/10;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String n1 = scn.nextLine();
+        String n2 = scn.nextLine();
+
+        String res = addStrings(n1, n2);
+        System.out.println(res);
+    }
+}
