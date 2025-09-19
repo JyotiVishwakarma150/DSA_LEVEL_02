5b590d48700efe63684a6cb1180ccd774a8cd58d
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-09-19 22:14:30 +0530

    ComplexNumberMultiplication

---
+++ src/_01_ArraysAndStrings/_29_ComplexNumberMultiplication.java
@@ -0,0 +1,25 @@
+package _01_ArraysAndStrings;
+import java.util.*;
+public class _29_ComplexNumberMultiplication {
+    // ~~~~~~~~~~~~~User's Section~~~~~~~~~~~~~
+    public static String complexNumberMultiply(String num1, String num2) {
+        // write your code here
+        //num1=a+bi
+        //num2=c+di;
+        //res=(a*c)-(b*d)+(a*d)+(c*b)i
+        int a=Integer.parseInt(num1.substring(0,num1.indexOf('+')));
+        int b=Integer.parseInt(num1.substring(num1.indexOf('+')+1,num1.length()-1));
+        int c=Integer.parseInt(num2.substring(0,num2.indexOf('+')));
+        int d=Integer.parseInt(num2.substring(num2.indexOf('+')+1,num2.length()-1));
+       return ""+(a*c - b*d) + "+" + (a*d + c*b) + "i";
+    }
+
+    // ~~~~~~~~~~~~Input Management~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String num1 = scn.nextLine();
+        String num2 = scn.nextLine();
+        String res = complexNumberMultiply(num1, num2);
+        System.out.println(res);
+    }
+}
