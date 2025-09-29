083efd24833c006e71a433feb075d199a9129dd1
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-09-30 00:00:16 +0530

    PascalsTriangle

---
+++ src/_01_ArraysAndStrings/_43_PascalsTriangle.java
@@ -0,0 +1,26 @@
+package _01_ArraysAndStrings;
+import java.util.*;
+public class _43_PascalsTriangle {
+    // ~~~~~~~~~~~User Section~~~~~~~~~~~~~~~~
+    public static ArrayList<Integer> pascalRow(int i) {
+        ArrayList<Integer> res=new ArrayList<>();
+        int val=1;
+        for(int j=0;j<=i;j++){
+            res.add(val);
+            val=val*(i-j)/(j+1);
+        }
+        return res;
+    }
+
+    // ~~~~~~~~~~~Input management~~~~~~~~~~~~~~~~
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        ArrayList<Integer> res = pascalRow(n);
+        for (int val : res) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
