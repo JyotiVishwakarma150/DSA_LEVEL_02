f71baa08d2143fcea4d647af33f5e0b073d5e28e
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-12-12 00:16:48 +0530

    CountOfSubstringsWithExactlyKUniqueCharacters

---
+++ src/_02_HashmapAndHeap/_15_MaximumConsecutiveOnesI.java
@@ -0,0 +1,39 @@
+package _02_HashmapAndHeap;
+import java.util.*;
+public class _15_MaximumConsecutiveOnesI {
+    public static int solution(int[] arr,int k){
+        // write your code here
+        int ans=0;
+        int j=-1;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                count++;
+            }
+            while(count>k){
+                j++;
+                if(arr[j]==0){
+                    count--;
+                }
+            }
+            int len=i-j;
+            if(len>ans){
+                ans=len;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i  < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        int k=scn.nextInt();
+        System.out.println(solution(arr,k));
+    }
+
+}
