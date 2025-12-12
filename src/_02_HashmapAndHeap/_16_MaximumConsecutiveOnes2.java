d26b27be06ad25541568f2dceb649ffaa36498b9
Jyoti Vishwakarma <jyotivishwakarma2708@gmail.com>
2025-12-13 00:05:31 +0530

    MaximumConsecutiveOnes2

---
+++ src/_02_HashmapAndHeap/_16_MaximumConsecutiveOnes2.java
@@ -0,0 +1,36 @@
+package _02_HashmapAndHeap;
+import java.util.*;
+public class _16_MaximumConsecutiveOnes2 {
+    public static int solution(int[] arr){
+        int ans=0;
+        int j=-1;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                count++;
+            }
+            while(count>1){
+                j++;
+                if(arr[j]==0){
+                    count--;
+                }
+            }
+            int len=i-j;
+            if(len>ans){
+                ans=len;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i  < n; i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
+    }
+}
